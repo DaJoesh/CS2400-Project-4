0fbb5c7e60b2ddfdb812014d95421d02c8cbc373
BuildTools <unconfigured@null.spigotmc.org>
2021-04-05 14:45:49 -0700

    First Attempt at reading file - Josh and Russell

--- MHDriver.java
+++ MHDriver.java
@@ -1,8 +1,43 @@
 //MaxHeap Driver By Joshua Jenkins and Russell Rickards
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.io.PrintWriter;
 public class MHDriver
 {
-
+    public static void main(String[] args)
+    {
+        try
+        {
+            int counter = 0;
+            File file = new File("data_random.txt");
+            if (!file.exists())
+            {
+                System.out.println("ERROR: file does not exist");
+                System.exit(0);
+            }
+            Scanner fileR = new Scanner(file);
+            Scanner fileZ = new Scanner(file);
+            int n;
+            while(fileZ.hasNext())
+            {
+                counter++;
+            }
+            int[] list = new int[counter];
+            counter = 0;
+            while(fileR.hasNext())
+            {
+                n = fileR.nextInt();
+                list[counter] = n;
+                counter++;
+            }
+            fileR.close();
+            fileZ.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
