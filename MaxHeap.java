dd3ea862831b5d387b25534c12c32708ee18085f
BuildTools <unconfigured@null.spigotmc.org>
2021-04-05 14:06:15 -0700

    up to reheap - Josh

--- MaxHeap.java
+++ MaxHeap.java
@@ -40,4 +40,90 @@ public final class MaxHeap<T extends Comparable<? super T>> implements MaxHeapIn
         for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
         reheap(rootIndex);
     } // end constructor
+    public void add(T newEntry)
+    {
+        checkIntegrity(); // Ensure initialization of data fields
+        int newIndex = lastIndex + 1;
+        int parentIndex = newIndex / 2;
+        while ( (parentIndex > 0) && newEntry.compareTo(heap[parentIndex]) > 0)
+        {
+            heap[newIndex] = heap[parentIndex];
+            newIndex = parentIndex;
+            parentIndex = newIndex / 2;
+        } // end while
+        heap[newIndex] = newEntry;
+        lastIndex++;
+        ensureCapacity();
+    } // end add
+
+    public T removeMax()
+    {
+
+        checkIntegrity(); // Ensure initialization of data fields
+        T root = null;
+        if (!isEmpty())
+        {
+            root = heap[1]; // Return value
+            heap[1] = heap[lastIndex]; // Form a semiheap
+            lastIndex--; // Decrease size
+            reheap(1); // Transform to a heap
+        } // end if
+        return root;
+    } // end removeMax
+
+    public T getMax()
+    {
+        checkIntegrity();
+        T root = null;
+        if (!isEmpty())
+            root = heap[1];
+        return root;
+    } // end getMax
+
+    public boolean isEmpty()
+    {
+        return lastIndex < 1;
+    } // end isEmpty
+
+    public int getSize()
+    {
+        return lastIndex;
+    } // end getSize
+
+    public void clear()
+    {
+        checkIntegrity();
+        while (lastIndex > -1)
+        {
+            heap[lastIndex] = null;
+            lastIndex--;
+        } // end while
+        lastIndex = 0;
+    } // end clear
+
+    private void reheap(int rootIndex)
+    {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex;
+        while (!done && (leftChildIndex <= lastIndex) )
+        {
+            int largerChildIndex = leftChildIndex; // Assume larger
+            int rightChildIndex = leftChildIndex + 1;
+            if ( (rightChildIndex <= lastIndex) &&
+                    heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+            {
+                largerChildIndex = rightChildIndex;
+            } // end if
+            if (orphan.compareTo(heap[largerChildIndex]) < 0)
+            {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex;
+            }
+            else
+                done = true;
+        } // end while
+        heap[rootIndex] = orphan;
+    } // end reheap
 }
