a5d062dfd475192d3c3ee16296bc01469a610233
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 16:37:35 -0700

    @supressWarnings  - Josh

--- MaxHeap.java
+++ MaxHeap.java
@@ -156,6 +156,7 @@ public final class MaxHeap<T extends Comparable<? super T>> implements MaxHeapIn
 
     public T[] getTen()
     {
+        @SuppressWarnings ("unchecked")
         T[] n =  (T[]) new Integer[10]; 
         for(int i = 0; i<n.length; i++)
         {
@@ -163,35 +164,6 @@ public final class MaxHeap<T extends Comparable<? super T>> implements MaxHeapIn
         }
         return n;
     }
-
-    /*public int reheapCounter(int rootIndex)
-    {
-        boolean done = false;
-        T orphan = heap[rootIndex];
-        int leftChildIndex = 2 * rootIndex;
-        int counter = 0;
-        while (!done && (leftChildIndex <= lastIndex) )
-        {
-            int largerChildIndex = leftChildIndex; // Assume larger
-            int rightChildIndex = leftChildIndex + 1;
-            if ( (rightChildIndex <= lastIndex) &&
-                    heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
-            {
-                largerChildIndex = rightChildIndex;
-            } // end if
-            if (orphan.compareTo(heap[largerChildIndex]) < 0)
-            {
-                heap[rootIndex] = heap[largerChildIndex];
-                rootIndex = largerChildIndex;
-                leftChildIndex = 2 * rootIndex;
-            }
-            else
-                done = true;
-        counter++;
-        } // end while
-        heap[rootIndex] = orphan;
-        return counter;
-    } // end reheapCounter*/
 
     // Throws an exception if the client requests a capacity that is too large.
     private void checkCapacity(int capacity)
